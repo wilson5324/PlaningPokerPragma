da456f66e95a5f7b546da7ab01a62e4582fd8400
Wilson <wilson5324@hotmail.com>
2025-01-16 22:51:35 -0500

    Implementar control value accesor

--- src/app/components/input-atom/input-atom.component.ts
+++ src/app/components/input-atom/input-atom.component.ts
@@ -5,120 +5,45 @@ import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR
   selector: 'input-atom',
   templateUrl: './input-atom.component.html',
   styleUrls: ['./input-atom.component.sass'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => InputAtomComponent),
-      multi: true
-    },
-    {
-      provide: NG_VALIDATORS,
-      useExisting: forwardRef(() => InputAtomComponent),
-      multi: true
-    }
-  ]
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputAtomComponent),
+    multi: true
+  }]
 })
-export class InputAtomComponent implements ControlValueAccessor, Validator {
+export class InputAtomComponent implements ControlValueAccessor {
 
   @Input() type = 'text';
   @Input() placeholder = '';
   @Input() label?: string;
+  @Input() minLength?: number;
+  @Input() maxLength?: number;
 
-  /**
-   * Deshabilitar el input
-   */
-  disabled = false;
 
-  /**
-   * Valor interno del control
-   */
-  private _value = '';
+  isDisable!: boolean;
+  currentValue = "";
 
-  /**
-   * Señal para manejar el estado de error
-   */
-  hasError = signal(false);
+  private onChange = (_: any) => { };
+  private onTouch = () => { };
 
-  /**
-   * Señal para manejar el mensaje de error
-   */
-  errorMessage = signal('');
-
-  /**
-   * Funciones que Angular inyecta para notificar cambios y "toque" del control
-   */
-  private onChange: (_: any) => void = () => { };
-  private onTouched: () => void = () => { };
-
-  /**
-   * Se invoca cuando el valor en el control cambia
-   */
-  onInput(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    this._value = input.value;
-    this.onChange(this._value);
+  changeText($event: any): void {
+    this.onChange($event.target.value)
   }
 
-  /**
-   * Se invoca cuando el control pierde el foco
-   */
-  onBlur(): void {
-    this.onTouched();
+  writeValue(value: string): void {
+    this.currentValue = value;
   }
 
-  /* ---------------------------------------------------
-   * Métodos de ControlValueAccessor
-   * ---------------------------------------------------
-   */
-  writeValue(value: any): void {
-    this._value = value ?? '';
-  }
-
-  registerOnChange(fn: (_: any) => void): void {
+  registerOnChange(fn: any): void {
     this.onChange = fn;
   }
 
-  registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
   }
 
   setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
+    this.isDisable = isDisabled
   }
 
-  /**
-   * Getter para exponer valor en la plantilla
-   */
-  get value(): string {
-    return this._value;
-  }
-
-  /* ---------------------------------------------------
-   * Métodos de Validator
-   * ---------------------------------------------------
-   */
-  validate(control: AbstractControl): ValidationErrors | null {
-    if (control?.errors) {
-      this.hasError.set(true);
-      this.errorMessage.set(this._getErrorMessage(control.errors));
-      return control.errors;
-    } else {
-      this.hasError.set(false);
-      this.errorMessage.set('');
-      return null;
-    }
-  }
-
-  private _getErrorMessage(errors: ValidationErrors): string {
-    console.log(errors);
-    
-    if (errors['required']) {
-      return 'Este campo es obligatorio.';
-    }
-    if (errors['email']) {
-      return 'Ingrese un correo electrónico válido.';
-    }
-    // Extiende con otros tipos de validación
-    return 'El valor es inválido.';
-  }
 }
--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent {
-  formGroup: FormGroup;
+  formGroup: FormGroup; //Revisar form control
 
   constructor(private fb: FormBuilder) {
     this.formGroup = this.fb.group({
@@ -17,10 +17,33 @@ export class LoginComponent {
           Validators.required,
           Validators.minLength(5),
           Validators.maxLength(20),
+          this.customValidator()
         ],
       ],
     });
   }
+
+  customValidator() {
+    return (control: AbstractControl) => {
+      const value = control.value || '';
+
+      if (/[_.*#\/-]/.test(value)) {
+        return { charError: 'No puede contener caracteres especiales como _,.*#/-' };
+      }
+
+      const numCount = (value.match(/\d/g) || []).length;
+      if (numCount > 3) {
+        return { lengthNumberError: 'No puede contener más de 3 números' };
+      }
+
+      if (/^\d+$/.test(value)) {
+        return { onlyNumberError: 'No puede ser solo números' };
+      }
+
+      return null;
+    };
+  }
+
 
   onSubmit() {
     console.log(this.formGroup);
