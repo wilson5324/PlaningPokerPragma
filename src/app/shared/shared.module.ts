2a1f868e32aaaf74190c29fda5dcf4be9a47f886
Wilson <wilson5324@hotmail.com>
2025-03-04 16:41:28 -0500

    Correccion nombramiento carpetas atomic

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InputAtomComponent } from '../components/input-atom/input-atom.component';
-import { ButtonAtomComponent } from '../components/button-atom/button-atom.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RadioButtonAtomComponent } from '../components/radio-button-atom/radio-button-atom.component';
+import { RadioButtonAtomComponent } from '../atomic-desing/atoms/radio-button-atom/radio-button-atom.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CardComponent } from './components/card/card.component';
 import { UserIconComponent } from './components/user-icon/user-icon.component';
+import { InputAtomComponent } from '../atomic-desing/atoms/input-atom/input-atom.component';
+import { ButtonAtomComponent } from '../atomic-desing/atoms/button-atom/button-atom.component';
 
 
 
